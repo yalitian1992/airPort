76fa4e847a3e473861b7b9ec12416c58b873ed58
yalitian1992 <tianyali1992@outlook.com>
2018-12-14 13:21:52 +0800

    test

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-module.exports = {
-  api: {
-    baseURL: 'http://103.10.87.52:17011/'
-    // baseURL: 'http://10.40.2.177:8090/'
-    // baseURL: 'http://10.40.2.178:8090/'
-    // baseURL: 'http://192.168.215.180:15521'
-  }
-}
